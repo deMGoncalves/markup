7324ddb6958571c0f4a0954899b7ed989faa9f3e
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-12 09:36:21 -0300

    fix(favicon): o type deve ser image/x-icon

--- src/icon.jsx
+++ src/icon.jsx
@@ -5,5 +5,5 @@ import render from './render'
 export default (schema) =>
   f.forEach(
     ['icon', 'shortcut', 'apple-touch-icon'],
-    rel => render(<link href={f.or(schema.icon, '/favicon.ico')} rel={rel} type='image/png' />)
+    rel => render(<link href={f.or(schema.icon, '/favicon.ico')} rel={rel} type='image/x-icon' />)
   )

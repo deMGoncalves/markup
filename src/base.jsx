5c8855b9c19bd65282755e23f385e9f7593e2b45
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-13 15:36:19 -0300

    feat(author): agora consigo obter o author global

--- src/base.jsx
+++ src/base.jsx
@@ -1,6 +1,7 @@
 import h from 'h'
 import * as f from 'f'
 import render from './render'
+import settings from './settings'
 
 export default (page) =>
-  render(<base href={f.or(page.base, '/')} />)
+  render(<base href={f.or(page.base, settings.base, '/')} />)

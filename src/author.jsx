d6a118ca17be775ad265cd525c45c1e119c88b93
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-13 15:35:09 -0300

    feat(author): inclusao do author global encontrado no settings

--- src/author.jsx
+++ src/author.jsx
@@ -1,6 +1,7 @@
 import h from 'h'
 import * as f from 'f'
 import render from './render'
+import settings from './settings'
 
 export default (page) =>
-  render(<meta name='author' content={f.or(page.author, '')} />)
+  render(<meta name='author' content={f.or(page.author, settings.author, '')} />)

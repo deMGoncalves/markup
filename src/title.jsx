f2fadefccb00122a7e7db40722b42d843b1d3318
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-13 15:40:03 -0300

    feat(title): agora posso pegar o title global como fallback

--- src/title.jsx
+++ src/title.jsx
@@ -1,6 +1,7 @@
 import h from 'h'
 import * as f from 'f'
 import render from './render'
+import settings from './settings'
 
 export default (page) =>
-  render(<title>{f.or(page.title, '')}</title>)
+  render(<title>{f.or(page.title, settings.title, '')}</title>)

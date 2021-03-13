a9c9141113ec449f7e331b2425d79240e9455116
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-03-13 15:38:04 -0300

    feat(description): agora posso pegar o description global como fallback

--- src/description.jsx
+++ src/description.jsx
@@ -1,6 +1,7 @@
 import h from 'h'
 import * as f from 'f'
 import render from './render'
+import settings from './settings'
 
 export default (page) =>
-  render(<meta name='description' content={f.or(page.description, '')} />)
+  render(<meta name='description' content={f.or(page.description, settings.description, '')} />)
